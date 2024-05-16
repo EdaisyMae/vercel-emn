5ac674b9c2e172ac346995c6b9c6571d115de09c
EdaisyMae <niroedaisymae@gmail.com>
2024-05-16 12:32:34 +0800

    1st commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>NIRO_PORTFOLIO</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
+.w3-row-padding img {margin-bottom: 12px}
+.bgimg {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url('https://scontent.fcrk2-1.fna.fbcdn.net/v/t1.15752-9/423541627_930773601562596_6949645545933169888_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG01ShrBQht29NvV-DjTQU57LodwHSZeCvsuh3AdJl4K27c2zRDbib_IvQhqA0ULWIXLn1fq4PE8O9BxedfMQah&_nc_ohc=OfqIoupsgOMQ7kNvgGYLlo-&_nc_ht=scontent.fcrk2-1.fna&oh=03_Q7cD1QFONZ0JpkLNEY8iT-GWLiZQDGGMUzIZ82yEHMMnnln0vg&oe=666AE66F');
+  min-height: 100%;
+}
+</style>
+</head>
+<body>
+
+<!-- Sidebar with image -->
+<nav class="w3-sidebar w3-hide-medium w3-hide-small" style="width:40%">
+  <div class="bgimg"></div>
+</nav>
+
+<!-- Hidden Sidebar (reveals when clicked on menu icon)-->
+<nav class="w3-sidebar w3-black w3-animate-right w3-xxlarge" style="display:none;padding-top:150px;right:0;z-index:2" id="mySidebar">
+  <a href="javascript:void(0)" onclick="closeNav()" class="w3-button w3-black w3-xxxlarge w3-display-topright" style="padding:0 12px;">
+    <i class="fa fa-remove"></i>
+  </a>
+  <div class="w3-bar-block w3-center">
+    <a href="#" class="w3-bar-item w3-button w3-text-grey w3-hover-black" onclick="closeNav()">Home</a>
+    <a href="#portfolio" class="w3-bar-item w3-button w3-text-grey w3-hover-black" onclick="closeNav()">Portfolio</a>
+    <a href="#about" class="w3-bar-item w3-button w3-text-grey w3-hover-black" onclick="closeNav()">About</a>
+    <a href="#contact" class="w3-bar-item w3-button w3-text-grey w3-hover-black" onclick="closeNav()">Contact</a>
+  </div>
+</nav>
+
+<!-- Page Content -->
+<div class="w3-main w3-padding-large" style="margin-left:40%">
+
+  <!-- Menu icon to open sidebar -->
+  <span class="w3-button w3-top w3-white w3-xxlarge w3-text-grey w3-hover-text-black" style="width:auto;right:0;" onclick="openNav()"><i class="fa fa-bars"></i></span>
+
+  <!-- Header -->
+  <header class="w3-container w3-center" style="padding:128px 16px" id="home">
+    <h1 class="w3-jumbo"><b>EDAISY MAE</b></h1>
+    <p>Photographer and Web Designer.</p>
+    <img src="https://scontent.fcrk2-1.fna.fbcdn.net/v/t1.15752-9/423541627_930773601562596_6949645545933169888_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG01ShrBQht29NvV-DjTQU57LodwHSZeCvsuh3AdJl4K27c2zRDbib_IvQhqA0ULWIXLn1fq4PE8O9BxedfMQah&_nc_ohc=OfqIoupsgOMQ7kNvgGYLlo-&_nc_ht=scontent.fcrk2-1.fna&oh=03_Q7cD1QFONZ0JpkLNEY8iT-GWLiZQDGGMUzIZ82yEHMMnnln0vg&oe=666AE66F" class="w3-image w3-hide-large w3-hide-medium w3-round" width="1000" height="1333">
+    <img src="https://scontent.fcrk2-1.fna.fbcdn.net/v/t1.15752-9/423541627_930773601562596_6949645545933169888_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG01ShrBQht29NvV-DjTQU57LodwHSZeCvsuh3AdJl4K27c2zRDbib_IvQhqA0ULWIXLn1fq4PE8O9BxedfMQah&_nc_ohc=OfqIoupsgOMQ7kNvgGYLlo-&_nc_ht=scontent.fcrk2-1.fna&oh=03_Q7cD1QFONZ0JpkLNEY8iT-GWLiZQDGGMUzIZ82yEHMMnnln0vg&oe=666AE66F" class="w3-image w3-hide-large w3-hide-medium w3-round" width="1000" height="1333">
+   
+     
+  </header>
+
+  <!-- Portfolio Section -->
+  <div class="w3-padding-32 w3-content" id="portfolio">
+    <h2 class="w3-text-grey">My Portfolio</h2>
+    <hr class="w3-opacity">
+
+    <!-- Grid for photos -->
+    <div class="w3-row-padding" style="margin:0 -16px">
+      <div class="w3-half">
+        <img src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.15752-9/438332825_1506655566556195_3227408999390266489_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFM6XC5Nldd-H17gmeBRk3SRPERvOROJ-JE8RG85E4n4nu8lg83dpM_LvzY4Hi2ADccYOU8_7Tc_eO9mAcM5NeP&_nc_ohc=NWksQnUamBIQ7kNvgH40sYz&_nc_ht=scontent.fcrk4-1.fna&oh=03_Q7cD1QFeHLhX3rVDlwkeF1m01Pz3uvPB-02GfQInWMZTY8Bl4g&oe=666B0D95" style="width:100%">
+        <img src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.15752-9/441505504_985557939826591_5070027095089132222_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFOTC5FDfO_l-DtTr70kHCU2j5LweafBf7aPkvB5p8F_jilf9WJFtN234ocLy-CEeVark7cK2q0toEiOYF7TRJV&_nc_ohc=Oy5-yb1B-LMQ7kNvgFcBvJg&_nc_ht=scontent.fcrk4-1.fna&oh=03_Q7cD1QFXx94KgetgmOF9MCOmpvIwViMo_rZrIBzrRfHnMi0uHA&oe=666AF79E" style="width:100%">
+        <img src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.15752-9/438293691_1648598259312002_3994913366075776027_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFukV3s6ckNjEkqAfhurHE4LUSGNWiUoSAtRIY1aJShIK2oyZYkhgMqCIva6Yq2IRAXwu10guIqHIv4c7CBKogl&_nc_ohc=TwGHKFaosFQQ7kNvgFEPJXU&_nc_ht=scontent.fcrk4-1.fna&oh=03_Q7cD1QFGywDBQfc2moaac6vCRRE_dWiZcwPtTTQmiCVcrqjZMA&oe=666AE022" style="width:100%">
+      </div>
+
+      <div class="w3-half">
+        <img src="https://scontent.fcrk2-2.fna.fbcdn.net/v/t1.15752-9/440771879_3288254668144851_1830004457216514939_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFIg_AD0vV7dKK1T8jvDgsWQEsnpysQjwpASyenKxCPCstMh3vOz6vGSc2RWt3i-bNOkKbgqwV3AwDTJqvQxr-w&_nc_ohc=NDeT4f6_At0Q7kNvgHbpWqA&_nc_ht=scontent.fcrk2-2.fna&oh=03_Q7cD1QFXb96rWxdv_v8-vKDt0ztZZDvPAZzJjxYuLrfJEn7fug&oe=666B04EB" style="width:100%">
+        <img src="https://scontent.fcrk2-2.fna.fbcdn.net/v/t1.15752-9/438270905_1991802837902244_139698236904342966_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGEcU42RnpX5bZweDbfqL8n6B6Shud0iJHoHpKG53SIkUjsQlsb56lbaLuGZZiEB-3j41a9ooIiB1YGGZ5AJsK5&_nc_ohc=7MM4v-2ieNQQ7kNvgFJEjrq&_nc_ht=scontent.fcrk2-2.fna&oh=03_Q7cD1QGZEFUlcNVNiNqq1AWbZZHkJIw80USpAtwer6uw02o8cw&oe=666AE90E" style="width:100%">
+        <img src="https://scontent.fcrk2-1.fna.fbcdn.net/v/t1.15752-9/438293609_943926007473283_2329288195051850902_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFL--7y4oapWh1CuDBBSRWJJ00uk3kacuonTS6TeRpy6nJ8KDTGt0pXEdMF8eoRSppJsMzrOgvTMhNylYGm-_Lg&_nc_ohc=P2U5DKF8gi4Q7kNvgFmlENH&_nc_ht=scontent.fcrk2-1.fna&oh=03_Q7cD1QEcMGLyERacMeXwh42gcINod-WlEvgBp5WMXuVNLDyryQ&oe=666AD4BB" style="width:100%">
+        <img src="https://scontent.fcrk2-1.fna.fbcdn.net/v/t1.15752-9/438267471_441017111850711_1752510504896447691_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHvt3Y1lq6ICPUYHncAsLkHDcw4k4Z5SnENzDiThnlKcfJ19LegZbJVSdW5GQxkfeQQcG2FPLr7THyDETkwlp1P&_nc_ohc=HwF2K2Pf1QQQ7kNvgFstDWl&_nc_ht=scontent.fcrk2-1.fna&oh=03_Q7cD1QEBKgrE9jpU-9MHFP3GYa6ARdIahtvQEEEfOSaRTydbmQ&oe=666B024E" style="width:100%">
+      </div>
+    <!-- End photo grid -->
+    </div>
+  <!-- End Portfolio Section -->
+  </div>
+
+  <!-- About Section -->
+  <div class="w3-content w3-justify w3-text-grey w3-padding-32" id="about">
+    <h2>About</h2>
+    <hr class="w3-opacity">
+    <p>Hey there! I’m Edaisy Mae Niro, a 3rd-year IT student on a mission to understand and conquer the digital world. I spend my days diving into the fascinating realms of computers, software, and all things tech.
+
+Ever since I got my hands on my first computer, I’ve been hooked. From fixing glitches to exploring new programming languages, every challenge is a chance to learn and grow. My journey in IT began with curiosity and a desire to unravel the mysteries behind the screens.
+
+In class, I’m mastering the building blocks of IT, from coding in languages like Java and Python to understanding networks and databases. But it’s not just about acing exams; it’s about grasping concepts that will shape the future of technology.
+
+When I’m not buried in code, you’ll find me tinkering with gadgets, gaming with friends, or simply soaking up inspiration from the digital universe. I believe that technology has the power to transform lives, and I’m determined to be part of that revolution.
+
+With each line of code I write and each problem I solve, I’m one step closer to becoming the IT wizard I aspire to be. Let’s connect and explore the endless possibilities of the digital frontier together!
+    </p>
+    <h3 class="w3-padding-16">My Skills</h3>
+    <p class="w3-wide">Photography</p>
+    <div class="w3-light-grey">
+      <div class="w3-container w3-center w3-padding-small w3-dark-grey" style="width:95%">95%</div>
+    </div>
+    <p class="w3-wide">Web Design</p>
+    <div class="w3-light-grey">
+      <div class="w3-container w3-center w3-padding-small w3-dark-grey" style="width:85%">85%</div>
+    </div>
+    <p class="w3-wide">Photoshop</p>
+    <div class="w3-light-grey">
+      <div class="w3-container w3-center w3-padding-small w3-dark-grey" style="width:80%">80%</div>
+    </div><br>
+
+    
+    
+
+   
+   
+    
+  <!-- End About Section -->
+  </div>
+
+  <!-- Contact Section -->
+  <div class="w3-padding-32 w3-content w3-text-grey" id="contact" style="margin-bottom:64px">
+    <h2>Contact Me</h2>
+    <hr class="w3-opacity">
+
+    <div class="w3-section">
+      <p><i class="fa fa-map-marker fa-fw w3-xxlarge w3-margin-right"></i> Luna, La Union</p>
+      <p><i class="fa fa-phone fa-fw w3-xxlarge w3-margin-right"></i> Phone: +00 151515</p>
+      <p><i class="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email: niroedaisymae@mail.com</p>
+    </div>
+    
+    <!-- Image of location/map -->
+    <img src="/w3images/map.jpg" class="w3-image w3-greyscale" style="width:100%;margin:32px 0">
+   
+    <p>Lets get in touch. Send me a message:</p>
+    <form action="/action_page.php" target="_blank">
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name"></p>
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Email" required name="Email"></p>
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Subject" required name="Subject"></p>
+      <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message" required name="Message"></p>
+      <p>
+        <button class="w3-button w3-light-grey w3-padding-large" type="submit">
+          <i class="fa fa-paper-plane"></i> SEND MESSAGE
+        </button>
+      </p>
+    </form>
+  <!-- End Contact Section -->
+  </div>  
+  
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-64 w3-light-grey w3-center w3-opacity w3-xlarge" style="margin:-24px">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+    <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+  <!-- End footer -->
+  </footer>
+  
+<!-- END PAGE CONTENT -->
+</div>
+
+<script>
+// Open and close sidebar
+function openNav() {
+  document.getElementById("mySidebar").style.width = "60%";
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+</script>
+
+</body>
+</html>
